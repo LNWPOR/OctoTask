9edf9eeecb4997c840c074c3e47c90b02b148a62
LNWPOR <teerapat0-0@hotmail.com>
2014-09-07 00:23:01 -0700

    สร้าง class GameController,สร้างขนาดหน้าจอ

---
+++ src/GM/GameController.java
@@ -0,0 +1,39 @@
+package GM;
+
+import java.util.concurrent.ExecutionException;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class GameController extends StateBasedGame{
+    public static int gameWidth=1024;
+    public static int gameHeight=720;
+    
+    
+    
+	public GameController(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			AppGameContainer app  = new AppGameContainer(new GameController("OctoTask"));
+			app.setDisplayMode(gameWidth,gameHeight, false);
+			app.start();
+		}catch(SlickException e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
