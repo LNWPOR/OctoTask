94effebb7a112d89c76caec3707dba28e0729a67
LNWPOR <teerapat0-0@hotmail.com>
2014-10-04 02:47:07 +0700

    ใส่เสียงเพิ่มเลือด ลดเลือด

--- src/gm/AudioGM.java
+++ src/gm/AudioGM.java
@@ -2,6 +2,7 @@ package gm;
 
 import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 
 public class AudioGM {
 
@@ -9,10 +10,16 @@ public class AudioGM {
 	public Music gameOverAD;
 	public Music gamePlayAD;
 	
+	public Sound damageAD;
+	public Sound increaseHPAD;
+	
 	public AudioGM() throws SlickException{
 		
 		menuAD = new Music("res/menuAD.wav");
 		gameOverAD = new Music("res/gameOverAD.wav");
 		gamePlayAD = new Music("res/gamePlayAD.wav");
+		
+		damageAD = new Sound("res/damage.wav");
+		increaseHPAD = new Sound("res/gainlp.wav");
 	}
 }
--- src/oj/HP.java
+++ src/oj/HP.java
@@ -1,16 +1,28 @@
 package oj;
 
+import org.newdawn.slick.SlickException;
+
+import gm.AudioGM;
+
 public class HP {
 	
 	
 	public static int hp ;
+	private AudioGM audioRef;
+	
+	public HP() throws SlickException{
+		audioRef = new AudioGM();
+	}
+	
 	
 	public void HpDamage(){
 		hp-=1;
+		audioRef.damageAD.play();
 	}
 	
 	public void HpIncrease(){
 		hp+=1;
+		audioRef.increaseHPAD.play();
 	}
 	
 }
